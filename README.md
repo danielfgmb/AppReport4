8b873347250eb4a1b90ac36b72f36987ce02d336
danielfgmb <49533662+danielfgmb@users.noreply.github.com>
2023-11-25 19:05:12 -0500

    Update & Deploy

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# AppReport4
+# App-Report 4
+We designed and deployed an interactive web page for showcase the App Report, includind the previous parts.
+
+## Link to Web
+The page is deployed using GitHub directly
+
+## Team:
+- Daniel Fernando Gómez Barrera
+- Daniel Andrés Bernal Cáceres
+- Juan Pablo Martínez Pineda
+
+## Repository Link
+- https://github.com/tachiyomiorg/tachiyomi
+
---
+++ docs/script.js
@@ -0,0 +1,69 @@
+// script.js
+const appImage = document.getElementById('app-image');
+
+// Function to change the app image
+function changeAppImage(newImageSrc) {
+  appImage.src = newImageSrc;
+}
+
+// Usage example: changeAppImage('new-image.jpg');
+// Add event listeners for interaction (example using a button click)
+const button = document.getElementById('your-button-id');
+
+button.addEventListener('click', function() {
+  // Change the app image when the button is clicked
+  changeAppImage('new-image.jpg');
+});
+
+
+function animation_xd($e, xd = true){
+  if(xd){
+    console.log("entre1")
+    
+    $($e).animate({
+      width: '100vh', // New width
+      height: '100vh', // New height
+    }, 3000, function () { console.log("entre_Dentro"); animation_xd($e, false) }); // Animation duration in milliseconds (1000ms = 1 second)
+  
+  }
+  else{
+    console.log("entre2")
+    $($e).animate({
+      width: '180vh', // New width
+      height: '140vh', // New height
+    }, 5000, function () { animation_xd($e, true)}); // Animation duration in milliseconds (1000ms = 1 second)
+    
+  }
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function random_perspective($e){
+  console.log("rp",showing)
+    if(!showing){
+      var numberInRange25 = getRandomNumber(-30, 30);
+      var numberInRange8 = getRandomNumber(-8, 8);
+      console.log("rp",numberInRange25,numberInRange8)
+    $($e).delay(20000)
+    .css("transform", 'rotateY('+numberInRange25+'deg) rotateX('+numberInRange8+'deg)')
+    .animate({
+      transform: 'rotateY('+numberInRange25+'deg) rotateX('+numberInRange8+'deg)', // New width
+    }, 1000, function () { random_perspective($e, true)}); // Animation duration in milliseconds (1000ms = 1 second)
+  }
+}
+
+function random_perspective_inmediato($e){
+  if(!showing){
+    var numberInRange25 = getRandomNumber(-30, 30);
+    var numberInRange8 = getRandomNumber(-8, 8);
+    console.log("rp",numberInRange25,numberInRange8)
+  $($e).css("transform", 'rotateY('+numberInRange25+'deg) rotateX('+numberInRange8+'deg)')
+}
+}
+
+
+
